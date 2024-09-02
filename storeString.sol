b6cdaea92f2c098efe3045d349e6b7135b2126be
Kousik R G <124642171+Khaosssss@users.noreply.github.com>
2024-09-03 00:27:21 +0530

    Update storeString.sol

--- storeString.sol
+++ storeString.sol
@@ -1,1 +1,81 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TokenMaster is ERC721 {
+    address public owner;
+    uint256 public totalOccasions;
+    uint256 public totalSupply;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    struct Occasion {
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
+    mapping(uint256 => Occasion) public occasions;
+    mapping(uint256 => mapping(uint256 => address)) public seatTaken;
+    mapping(uint256 => uint256[]) public seatsTaken;
+    mapping(uint256 => mapping(address => bool)) public hasBought;
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+        owner = msg.sender;
+    }
+
+    function list(
+        string memory _name,
+        uint256 _cost,
+        uint256 _tickets,
+        uint256 _maxTickets,
+        string memory _date,
+        string memory _time,
+        string memory _location
+    ) public onlyOwner {
+        totalOccasions++;
+        occasions[totalOccasions] = Occasion(
+            totalOccasions,
+            _name,
+            _cost,
+            _tickets,
+            _maxTickets,
+            _date,
+            _time,
+            _location
+        );
+    }
+
+    function mint(uint256 _id, uint256 _seat) public payable {
+        Occasion storage occasion = occasions[_id];
+        require(occasion.tickets > 0, "No tickets left for this occasion");
+        require(msg.value >= occasion.cost, "Insufficient funds to mint ticket");
+        require(seatTaken[_id][_seat] == address(0), "This seat is already taken");
+        require(!hasBought[_id][msg.sender], "You have already bought a ticket for this occasion");
+
+        occasion.tickets--;
+        hasBought[_id][msg.sender] = true;
+        seatTaken[_id][_seat] = msg.sender;
+        seatsTaken[_id].push(_seat);
+        totalSupply++;
+        _safeMint(msg.sender, totalSupply);
+    }
+
+    function getOccasion(uint256 _id) public view returns (Occasion memory) {
+        return occasions[_id];
+    }
+
+    function withdraw() public onlyOwner{
+        (bool success,) = owner.call{value: address(this).balance}("");
+        require(success, "Failed to withdraw funds");
+    }
+}
